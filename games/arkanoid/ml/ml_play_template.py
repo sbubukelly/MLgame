9dde017337257f279995d9bec8c1e1156f84b17c
Kelly <sbubukellyg@gmail.com>
2021-03-11 21:17:31 +0800

    add smth

--- games/arkanoid/ml/ml_play_template.py
+++ games/arkanoid/ml/ml_play_template.py
@@ -4,6 +4,13 @@ The template of the main script of the machine learning process
 import math
 
 class MLPlay:
+
+    old_x = 0
+    old_y = 0
+    m = 0
+    predict_x = 0
+    predict_y = 0
+
     def __init__(self):
         """
         Constructor
@@ -19,40 +26,40 @@ class MLPlay:
             scene_info["status"] == "GAME_PASS"):
             return "RESET"
 
-        old_x = 0
-        old_y = 0 
-        m = 0
-        predict_x = 0
-        predict_y = 0
+        
         #先計算會不會撞到牆 然後算撞到牆後的落點修正 然後重新算斜率 然後再預測落點
         #先算低一個地圖
 
         if not self.ball_served:
             self.ball_served = True
-            command = "SERVE_TO_RIGHT"
-            old_x,old_y = scene_info["ball"] 
+            command = "SERVE_TO_RIGHT"  
+            self.old_x,self.old_y = scene_info["ball"] 
         else:
             x,y = scene_info["ball"]
-            if y>60 :
-                if x-old_x != 0:
-                    m = (y - old_y)/(x - old_x)
-                    #3/4 斜率不知為啥一直是正的
-                    if m > 0:      #to the right
+            #if x == 0:
+            #   print(x,y,self.old_x,self.old_y)
+            if y>60 and  y - self.old_y > 0:
+                if x-self.old_x != 0:
+                    self.m = (y - self.old_y)/(x - self.old_x)
+                    #print("m:",m)
+                    if self.m > 0:      #to the right
                         #predict_y = math.floor(200*m - 5)
-                        predict_y = math.floor((200-x)*m + y -5)
-                        predict_x = 195 
-                    elif m < 0:    #to rhe left
-                        print("hello")
-                        predict_x = 0
-                        predict_y = (-x)*m + y -5
-            old_x = x
-            old_y = y
-            if x >= 195 or x ==0:
-               print(predict_y,y,predict_x,x)
-            #print(old_x,old_y,x,y,m)
-            #print(x)
+                        self.predict_y = math.floor((200-x)*self.m + y -5)
+                        self.predict_x = 195 
+                    elif self.m < 0:    #to the left
+                        self.predict_x = 0
+                        self.predict_y = (0-x)*self.m + y
+                #if (x == 0 or x == 195):
+                #    print(self.predict_x,self.predict_y,x,y,self.m)
+            self.old_x = x
+            self.old_y = y
+           
+            #print(self.predict_x,self.predict_y,x,y)
             
-            command = "MOVE_LEFT"
+            if(scene_info["platform"][0] >= self.predict_x):
+                command = "MOVE_LEFT"
+            else:
+                command = "MOVE_RIGHT"
             #print(scene_info)
 
         return command
