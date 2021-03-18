435e273aa0badc032cdba1a220041bc1791b2c5a
sbubukelly <sbubukellyg@gmail.com>
2021-03-18 15:08:17 +0800

    add smth

--- games/arkanoid/ml/ml_play_template.py
+++ games/arkanoid/ml/ml_play_template.py
@@ -9,7 +9,7 @@ class MLPlay:
     old_y = 0
     m = 0
     predict_x = 0
-    predict_y = 0
+    predict_y = 395
 
     def __init__(self):
         """
@@ -27,8 +27,7 @@ class MLPlay:
             return "RESET"
 
         
-        #先計算會不會撞到牆 然後算撞到牆後的落點修正 然後重新算斜率 然後再預測落點
-        #先算低一個地圖
+    
 
         if not self.ball_served:
             self.ball_served = True
@@ -38,10 +37,10 @@ class MLPlay:
             x,y = scene_info["ball"]
             #if x == 0:
             #   print(x,y,self.old_x,self.old_y)
-            if y>60 and  y - self.old_y > 0:
+            if y - self.old_y > 0:
                 if x-self.old_x != 0:
                     self.m = (y - self.old_y)/(x - self.old_x)
-                    #print("m:",m)
+                    '''
                     if self.m > 0:      #to the right
                         #predict_y = math.floor(200*m - 5)
                         self.predict_y = math.floor((200-x)*self.m + y -5)
@@ -49,17 +48,35 @@ class MLPlay:
                     elif self.m < 0:    #to the left
                         self.predict_x = 0
                         self.predict_y = (0-x)*self.m + y
-                #if (x == 0 or x == 195):
-                #    print(self.predict_x,self.predict_y,x,y,self.m)
+                    '''
+                    self.predict_x = (395-y+self.m*x)/self.m
+                    '''
+                    if(scene_info["platform"][0]+20 >= self.predict_x):
+                        command = "MOVE_LEFT"
+                    elif(scene_info["platform"][0]+20 < self.predict_x):
+                        command = "MOVE_RIGHT"
+                    '''
+                    dist = scene_info["platform"][0]+20 - self.predict_x
+                    if(abs(dist) >= 0 ):
+                        if(dist < 0):           #ball on right side
+                            command = "MOVE_RIGHT"
+                        else:
+                            command = "MOVE_LEFT"
+
+                    else:
+                        command = "NONE"
+            else:
+                if(scene_info["platform"][0]+20 >= x):
+                        command = "MOVE_LEFT"
+                elif(scene_info["platform"][0]+20 < x):
+                        command = "MOVE_RIGHT"
+
             self.old_x = x
             self.old_y = y
            
             #print(self.predict_x,self.predict_y,x,y)
             
-            if(scene_info["platform"][0] >= self.predict_x):
-                command = "MOVE_LEFT"
-            else:
-                command = "MOVE_RIGHT"
+            
             #print(scene_info)
 
         return command
