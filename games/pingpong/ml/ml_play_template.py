8e6c525342eb8988c3ba0a76d9ad2ba61052449e
sbubukelly <sbubukellyg@gmail.com>
2021-05-03 18:06:04 +0800

    add stuff

--- games/pingpong/ml/ml_play_template.py
+++ games/pingpong/ml/ml_play_template.py
@@ -1,8 +1,16 @@
 """
 The template of the script for the machine learning process in game pingpong
 """
+import math
 
 class MLPlay:
+    frame_count = 0
+    old_x = 0
+    old_y = 0
+    m = 0
+    predict_x = 0
+    predict_y = 0
+    is_first = 1
     def __init__(self, side):
         """
         Constructor
@@ -11,7 +19,7 @@ class MLPlay:
                which side.
         """
         self.ball_served = False
-        self.side = side
+        self.side = "1P"
 
     def update(self, scene_info):
         """
@@ -23,8 +31,43 @@ class MLPlay:
         if not self.ball_served:
             self.ball_served = True
             return "SERVE_TO_LEFT"
+
         else:
-            return "MOVE_LEFT"
+            x,y = scene_info["ball"]
+            if y - self.old_y > 0 :
+                if x-self.old_x != 0:
+                    self.m = (y - self.old_y)/(x - self.old_x)
+                    self.predict_x = math.floor((390-y+self.m*x)/self.m)
+                    if(self.predict_x >= 195):
+                        print("hit right wall")
+                        self.predict_y = math.floor((195-x)*self.m + y -5)      #撞牆壁時的y
+                        self.predict_x = (390-self.predict_y+(-self.m)*195)/(-self.m)
+                        print(self.predict_x)
+                    elif(self.predict_x <= 0):
+                        print("hit left wall")
+                        self.predict_y = math.floor((0-x)*self.m + y -5)      #撞牆壁時的y
+                        self.predict_x = (390-self.predict_y+(-self.m)*0)/(-self.m)
+                        print(self.predict_x)
+                    
+                        #self.predict_x = (395-y+self.m*x)/self.m
+                
+                    #print(self.predict_x,self.predict_y,x,y)
+                
+                    if(scene_info["platform_1P"][0] + 40 > self.predict_x):
+                        command = "MOVE_LEFT"
+                    elif(scene_info["platform_1P"][0] + 40 < self.predict_x):
+                        command = "MOVE_RIGHT"
+                    else:
+                        command = "NONE"
+            else:
+                command = "NONE"
+            #print(scene_info)
+            self.old_x = x
+            self.old_y = y
+           
+
+        return command
+
 
     def reset(self):
         """
